f9f04767c4a45d95cc8c08f486a7b18f8002c1e7
Matjaxon <MattRJackson1@gmail.com>
2016-09-12 20:35:57 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./lib/tower_defense.js",
+  output: {
+  	filename: "./lib/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ["es2015"]
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
